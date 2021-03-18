37f23a4cec9ffbe7c3ccf04b31dac19aef3dbc36
Yitzy Bratt <yitzybratt32@gmail.com>
2021-03-18 11:36:08 -0400

    completes stringsPractice

--- 2stringsPractice.js
+++ 2stringsPractice.js
@@ -1,22 +1,97 @@
 // # 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
 
+// const firstName = "Larry";
+// const lastName = "Doomstien";
+
+// console.log(firstName + " " + lastName);
+
 // # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+// const firstName = "Larry";
+// const lastName = "Doomstien";
+
+// console.log(`${firstName} ${lastName}`);
 
 // # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
 
+// const readline = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+ 
+// readline.question('Please input a word.', word => {
+//   if (word === "marco") {
+//     console.log("polo!");
+//   }
+//   readline.close();
+// });
+
 // # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+
+// const colorOne = "white";
+// const colorTwo = "black";
+// const colorThree = "brown";
+
+// console.log("We saw the" + " " + colorOne + " " + "and" +  " " + colorTwo + " " + "dog, so we fed it some" + " " + colorThree + "food");
 
 // # 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
 
+// const colorOne = "white";
+// const colorTwo = "black";
+// const colorThree = "brown";
+
+// console.log(`We saw the ${colorOne} and ${colorTwo} dog, so we fed it some ${colorThree} food.`);
+
 // # 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+
+// const readline = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+ 
+// readline.question('What is your name?', name => {
+//   if (name !== "santa") {
+//     console.log("You're not Santa.");
+//   }
+//   readline.close();
+// });
 
 // # 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
 
+// const author = "Dr. Seuss";
+// const title = "The Cat in the Hat";
+
+// console.log(author + " " + "wrote the book:" + " " + title);
+
 // # 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+
+// const author = "Dr. Seuss";
+// const title = "The Cat in the Hat";
+
+// console.log(`${author} wrote the book: ${title}`);
 
 // # 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
 
+// const readline = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+ 
+// readline.question('Please enter a password.', password => {
+//   if (password === "Joshua") {
+//     console.log("Shall we play a game?");
+//   } else {
+//     console.log("access denied");
+//   }
+//   readline.close();
+// });
+
 // # 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
 
+// const cityOne = "New York"; 
+// const cityTwo = "New Jersey";
+// const cityThree = "Connecticut";
+
+// console.log(cityOne + ", " + cityTwo + " and " + cityThree + " make up the tristate area")
 
 // # SOLUTIONS: https://gist.github.com/peterxjang/79ba5acc912a86cce2a18c2f14b1c712
