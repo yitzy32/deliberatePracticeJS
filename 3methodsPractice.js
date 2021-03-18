cf6d623c76b88d28543ef57f7ff3b56b2007a2ba
Yitzy Bratt <yitzybratt32@gmail.com>
2021-03-18 09:34:39 -0400

    adds 7 js files

---
+++ 3methodsPractice.js
@@ -0,0 +1,22 @@
+// # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
+
+// # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
+// # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+
+// # 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+
+// # 5. Write a program that asks the user to enter a number, then prints the number times 10.
+
+// # 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
+
+// # 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+
+// # 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
+
+// # 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
+
+// # 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+
+
+// # SOLUTIONS: https://gist.github.com/peterxjang/1539a3ad79728ba4fb68dd8d07279c29
