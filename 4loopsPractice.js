8782894e0aad8350e1814e3f8819672de8fca3bf
Yitzy Bratt <yitzybratt32@gmail.com>
2021-03-20 23:04:28 -0400

    completes 4loopsPractice, adds index.html in order to  use prompt in browser

--- 4loopsPractice.js
+++ 4loopsPractice.js
@@ -1,22 +1,125 @@
 // # 1. Write a while loop to print the numbers 1 through 10.
 
+// let number = 1;
+
+// while (number <= 10) {
+//   console.log(number);
+//   number++;
+// }
+
+// let numbers = [1,2,3,4,5,6,7,8,9,10];
+
+// numbers.forEach(function(number) {
+//   console.log(number);
+// });
+
+// for (let number of numbers) {
+//   console.log(number);
+// }
+
 // # 2. Write a while loop that prints the word "hello" 5 times.
+// let string = "hello";
+
+
+// let i = 0
+// while (i < 5) {
+//   console.log(string);
+//   i++;
+// }
+  
+// let strings = ["hello", "hello", "hello", "hello", "hello"];
+
+// strings.forEach(function(string) {
+//   console.log(string);
+// });
+
+// for (let string of strings) {
+//   console.log(string);
+// }
 
 // # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+// ------------------Runs in console in browser----------------
+
+// while (true) {
+//   let word = prompt("Please enter a word");
+//   if (word === "stop") {
+//     break;
+//   }
+// }
+
 
 // # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
 
+// let number = 5;
+
+// while (number <= 100) {
+//   console.log(number);
+//   number += 5;
+// }
+
 // # 5. Write a while loop that prints the number 9000 ten times.
 
+// let i = 0;
+
+// while (i < 10) {
+//   console.log(9000);
+//   i++;
+// }
+
+// let numbers = [9000, 9000, 9000, 9000, 9000, 9000, 9000, 9000, 9000, 9000]
+
+// numbers.forEach(function(number) {
+//   console.log(number);
+// });
+
+// for (let number of numbers) {
+//   console.log(number);
+// }
 // # 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+
+// while (true) {
+//   let input = prompt("Please enter a number");
+//   if (parseInt(input) > 10) {
+//     break;
+//   }
+// }
+
 
 // # 7. Write a while loop that prints the numbers 50 to 70.
 
+// let number = 50;
+
+// while (number <= 70) {
+//   console.log(number);
+//   number++;
+// }
+
 // # 8. Write a while loop that prints the phrase "Around the world" 144 times.
+
+// let string = "Around the world";
+
+// let i = 0;
+// while (i < 144) {
+//   console.log("string");
+//   i++;
+// }
 
 // # 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
 
+// while (true) {
+//   let word = prompt("Please enter a word");
+//   if (word.length > 5) {
+//     break;
+//   } 
+// }
+
 // # 10. Write a while loop that prints the even numbers from 2 to 40.
 
+// let number = 2;
+
+// while (number <= 40) {
+//   console.log(number);
+//   number += 2;
+// }
 
 // # SOLUTIONS: https://gist.github.com/peterxjang/c4ec0e0f8f6e123d65ada9bf3100068b
