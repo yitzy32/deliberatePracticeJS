3140a98e0e974a037cb1b0e283f973113293d065
Yitzy Bratt <yitzybratt32@gmail.com>
2021-03-20 21:53:18 -0400

    completes methodsPractice (come back to three questions)

--- 3methodsPractice.js
+++ 3methodsPractice.js
@@ -1,22 +1,131 @@
 // # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
 
+// const readline = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+ 
+// readline.question('Please enter a word', word => {
+//   console.log(word.toUpperCase());
+//   readline.close();
+// });
+
 // # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
 
+// const readline = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+ 
+// readline.question('Please enter a number', number => {
+//   if (number > 100) {
+//     console.log('Thats a big number');
+// }
+//   readline.close();
+// });
+
 // # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+// -----------------COME BACK HERE---------------
+// const readline = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+ 
+// readline.question('Please enter a number', number1 => {
+//   console.log(parseInt(number1, 10) + 1);
+//   readline.close();
+// });
 
 // # 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
 
+// const readline = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+  
+// readline.question('Please enter a word', word => {
+//   console.log(`Here is your word backwards: ${word.split("").reverse().join("")}!`);
+//   readline.close();
+// });
+
 // # 5. Write a program that asks the user to enter a number, then prints the number times 10.
 
+// const readline = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+ 
+// readline.question('Please enter a number', number => {
+//   console.log(parseInt(number) * 10);
+//   readline.close();
+// });
+
 // # 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
+// -----------------COME BACK HERE---------------
+
+// const readline = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+ 
+// readline.question('Please enter two words.', words => {
+//   console.log(words.toUpperCase());
+//   readline.close();
+// });
 
 // # 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
 
+// const readline = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+ 
+// readline.question('Please enter a word', word => {
+//   console.log(word.length);
+//   readline.close();
+// });
+
 // # 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
+
+// const readline = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+ 
+// readline.question('Enter a number! Pretty Pretty Please.', number => {
+//   if (number < 0) {
+//     console.log("Thats a negative number");
+//   }
+//   readline.close();
+// });
 
 // # 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
 
+// -----------------COME BACK HERE---------------
+
+// const readline = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+ 
+// readline.question('Please enter two numbers.', number => {
+//   let number1 = parseInt(number);
+//   console.log(number1);
+//   readline.close();
+// });
+
 // # 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
 
+// const readline = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+ 
+// readline.question('Please enter a word', word => {
+//   if (word.length > 5) {
+//     console.log("Thats a long word");
+//   }
+//   readline.close();
+// });
 
 // # SOLUTIONS: https://gist.github.com/peterxjang/1539a3ad79728ba4fb68dd8d07279c29
