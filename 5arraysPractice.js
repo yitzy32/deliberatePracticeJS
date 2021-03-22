5d0784a7731773738a0f961a497251e0ec5e1076
Yitzy Bratt <yitzybratt32@gmail.com>
2021-03-21 21:51:38 -0400

    completes 5arraysPractice

--- 5arraysPractice.js
+++ 5arraysPractice.js
@@ -1,22 +1,114 @@
 // # 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
 
+// let strings = ["table", "chair", "cabinet"];
+
+// strings.push("couch", "lazy boy");
+
+// console.log(strings);
+
 // # 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+
+// let items = ["a", "b", "c", "d"];
+
+// items[1] = 2;
+
+// console.log(items);
 
 // # 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
 
+// let numbers = [1,2,3,4,5];
+
+// let i = 0;
+
+// while (i < numbers.length) {
+//   console.log(numbers[i]);
+//   i++;
+// }
+//   --------------forEach--------------
+// numbers.forEach(function(number) {
+//   console.log(number);
+// });
+// ----------------for of---------------
+
+// for (let number of numbers) {
+//   console.log(number);
+// }
+
 // # 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
+
+// let numbers = [1];
+
+// numbers.push(2,3,4);
+
+// console.log(numbers);
 
 // # 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
 
+// let strings = ["hello", "how", "are you doing?"];
+
+// strings[2] = strings[2].toUpperCase();
+
+// console.log(strings);
+
 // # 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+
+// let names = ["bill", "jill", "dill"];
+
+// let i = 0;
+// while (i < names.length) {
+//   console.log(names[i]);
+//   i++;
+// }
+
+// names.forEach(function(name){
+//   console.log(name)
+// })
+
+// for (let name of names) {
+//   console.log(name);
+// }
 
 // # 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
 
+// let strings = ["hello", "there"];
+
+// strings.push("young moose!");
+
+// console.log(strings);
+
 // # 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
+
+// let numbers = [9,7,5,4,3]
+
+// numbers[0] = numbers[0] * 10;
+
+// console.log(numbers);
 
 // # 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
 
+// let numbers = [1,2];
+
+// let i = 0;
+
+// while (i < numbers.length) {
+//   console.log(numbers[i]);
+//   i++;
+// }
+
+// numbers.forEach(function(number) {
+//   console.log(number);
+// });
+
+// for (let number of numbers) {
+//   console.log(number);
+// }
+
 // # 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
 
+// let countries = ["china", "india", "united states"]
+
+// countries.push("indonesia");
+
+// console.log(countries);
 
 // # SOLUTIONS: https://gist.github.com/peterxjang/7095a2b19e1da2cc18d4a0dcd66cb8f1
